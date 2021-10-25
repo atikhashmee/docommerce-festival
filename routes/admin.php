085aad665bde31c6e0de286a2d5d3f67e6362e2a
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-10-25 17:14:37 +0600

    admin update

--- routes/admin.php
+++ routes/admin.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 // Auth routes
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
     // Authentication Routes...
-    Auth::routes();
+    Auth::routes(['register' => false, 'reset' => false]);
     
     Route::group(['middleware' => 'auth:admin'], function() {
         Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'dashboard'])->name('dashboard');
