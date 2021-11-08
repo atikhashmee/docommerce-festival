ae811df47f7add42b20b6b262943b7db659ad57c
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-11-08 15:58:38 +0600

    Update MainController.php

--- app/Http/Controllers/Api/MainController.php
+++ app/Http/Controllers/Api/MainController.php
@@ -12,7 +12,7 @@ class MainController extends Controller
 {
     public function getOrders(Request $request, $store_id)
     {
-        $orders = Order::with('orderDetails')->select('orders.*')
+        $orders = Order::with('orderDetails', 'user', 'address')->select('orders.*')
         ->leftJoin("order_details", "order_details.order_id", "=", "orders.id")
         ->where("order_details.original_store_id", $store_id)
         ->get();
