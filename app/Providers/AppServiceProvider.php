ffffec4c4dfc954cfc63057907f08231285a6782
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-11-11 02:14:51 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,10 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
         view()->composer('layouts._menu', function ($view)  {
             $festival = getFestival();
-            $view->with('categories', \App\Models\Category::join('category_festivals', 'category_festivals.category_id', '=', 'categories.id')
+            $view->with('categories', \App\Models\Category::select("categories.*")
+            ->join('category_festivals', 'category_festivals.category_id', '=', 'categories.id')
+            ->leftJoin(\DB::raw("(SELECT COUNT(id) as total_product, category_id FROM products GROUP BY category_id) as P"), "P.category_id", "=", "categories.id")
+            ->where("P.total_product", ">", 0)
             ->where('category_festivals.festival_id', $festival->id)->get());
         });
     }
