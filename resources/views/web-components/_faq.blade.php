2c7ef7a2ad4d73cabe2398ebaecc26c52ffc42ad
Tusher <mtitmamun@gmail.com>
2021-11-10 19:49:35 +0600

    stockout badge

--- resources/views/web-components/_faq.blade.php
+++ resources/views/web-components/_faq.blade.php
@@ -25,14 +25,14 @@
                         <div class="card-header" id="headingOne">
                             <h2 class="mb-0">
                             <button class="btn btn-block text-left faq-btn" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                Q1: When this festival will start?
+                                Q1: Do I need to register to place an order?
                             </button>
                             </h2>
                         </div>
                     
                         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#festivalFAQ">
                             <div class="card-body">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nesciunt exercitationem, a iure consequuntur accusamus ex eligendi autem quod blanditiis dolores aut laboriosam ut laborum sequi hic velit sint numquam.
+                                Ans: No. You will have to verify your mobile number with OTP (One Time Password) before placing an order.
                             </div>
                         </div>
                         </div>
@@ -40,13 +40,13 @@
                         <div class="card-header" id="headingTwo">
                             <h2 class="mb-0">
                             <button class="btn btn-block text-left faq-btn collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Q2: When can I view the discounted prices?
+                                Q2: How to track order?
                             </button>
                             </h2>
                         </div>
                         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#festivalFAQ">
                             <div class="card-body">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nesciunt exercitationem, a iure consequuntur accusamus ex eligendi autem quod blanditiis dolores aut laboriosam ut laborum sequi hic velit sint numquam.
+                                Ans: Click on "Track Order" link on footer at <a href="https://festival.docommerce.com" target="_blank">https://festival.docommerce.com</a>, verify your mobile number (using which you placed order) with OTP (One Time Password) and you will be redirected to your order list. You can track your order status there.
                             </div>
                         </div>
                         </div>
@@ -54,16 +54,92 @@
                         <div class="card-header" id="headingThree">
                             <h2 class="mb-0">
                             <button class="btn btn-block text-left faq-btn collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Q3: How can I be sure that I get the maximum discounts?
+                                Q3: What is the delivery timeline?
                             </button>
                             </h2>
                         </div>
                         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#festivalFAQ">
                             <div class="card-body">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nesciunt exercitationem, a iure consequuntur accusamus ex eligendi autem quod blanditiis dolores aut laboriosam ut laborum sequi hic velit sint numquam.
+                                Ans: Within Dhaka Metropolitan City, delivery will be done by 24-72 hours.
                             </div>
                         </div>
                         </div>
+
+                        <div class="card">
+                            <div class="card-header" id="heading4">
+                                <h2 class="mb-0">
+                                <button class="btn btn-block text-left faq-btn collapsed" type="button" data-toggle="collapse" data-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+                                    Q4: How to cancel order?
+                                </button>
+                                </h2>
+                            </div>
+                            <div id="collapse4" class="collapse" aria-labelledby="heading4" data-parent="#festivalFAQ">
+                                <div class="card-body">
+                                    Ans: Click on "Track Order" link on footer at <a href="https://festival.docommerce.com">https://festival.docommerce.com</a>, verify your mobile number (using which you placed order) with OTP (One Time Password) and you will be redirected to your order list. You can cancel any order within 12 hours after placing the order.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-header" id="heading5">
+                                <h2 class="mb-0">
+                                <button class="btn btn-block text-left faq-btn collapsed" type="button" data-toggle="collapse" data-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
+                                    Q5: How to pay?
+                                </button>
+                                </h2>
+                            </div>
+                            <div id="collapse5" class="collapse" aria-labelledby="heading5" data-parent="#festivalFAQ">
+                                <div class="card-body">
+                                    Ans: "DoCommerce 11-11 Festival 2021" allows only "Cash on Delivery" orders. Our delivery agent will deliver your order at your door step within 72 hours. Check your products first and then pay to the delivery agent as per invoice.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-header" id="heading6">
+                                <h2 class="mb-0">
+                                <button class="btn btn-block text-left faq-btn collapsed" type="button" data-toggle="collapse" data-target="#collapse6" aria-expanded="false" aria-controls="collapse6">
+                                    Q6: What is the return and refund policy?
+                                </button>
+                                </h2>
+                            </div>
+                            <div id="collapse6" class="collapse" aria-labelledby="heading6" data-parent="#festivalFAQ">
+                                <div class="card-body">
+                                    Ans: After getting your order delivered, check your products first. If the product does not match with your order, you are most welcome to return that via our delivery agent. Once you pay, no return or refund policy would be applicable.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-header" id="heading7">
+                                <h2 class="mb-0">
+                                <button class="btn btn-block text-left faq-btn collapsed" type="button" data-toggle="collapse" data-target="#collapse7" aria-expanded="false" aria-controls="collapse7">
+                                    Q7: What is the festival duration?
+                                </button>
+                                </h2>
+                            </div>
+                            <div id="collapse7" class="collapse" aria-labelledby="heading7" data-parent="#festivalFAQ">
+                                <div class="card-body">
+                                    Ans: "DoCommerce 11-11 Festival 2021" will run from 12:00 am 11/11/2021 to 11:59 pm 17/11/2021.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-header" id="heading8">
+                                <h2 class="mb-0">
+                                <button class="btn btn-block text-left faq-btn collapsed" type="button" data-toggle="collapse" data-target="#collapse8" aria-expanded="false" aria-controls="collapse8">
+                                    Q8: What does DoCommerce do?
+                                </button>
+                                </h2>
+                            </div>
+                            <div id="collapse8" class="collapse" aria-labelledby="heading8" data-parent="#festivalFAQ">
+                                <div class="card-body">
+                                    Ans: Please visit <a href="https://docommerce.com" target="_blank">https://docommerce.com</a> to learn about DoCommerce.
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
                 <div class="tab-pane fade" id="pills-ban" role="tabpanel" aria-labelledby="pills-ban-tab">
