8e90deb72829adab87982c42374ef6dcf89ff099
Tusher <mtitmamun@gmail.com>
2021-11-14 15:14:23 +0600

    product page modal

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -196,11 +196,12 @@
                                 </div>
                             </div> 
                             
-                        </form>
+                        
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                         <button type="submit" class="btn btn-primary">Save changes</button>
+                    </form>
                     </div>
                 </div>
             
