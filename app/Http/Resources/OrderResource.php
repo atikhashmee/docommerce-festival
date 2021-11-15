7af51b78c197557a9c03e8331a3407844c795b81
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-11-15 19:38:58 +0600

    api update

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -14,11 +14,28 @@ class OrderResource extends JsonResource
      */
     public function toArray($request)
     {
-       return parent::toArray($request);
-    }
-    public function with($request) {
         return [
-            'version' => '1.0'
+            'order_number' => strtotime($this->order_number),
+            'user_id' => $this->user_id,
+            'discount_code' => $this->discount_code,
+            'sub_total' => $this->sub_total,
+            'discount_amount' => $this->discount_amount,
+            'total_shippings_charge' => $this->total_shippings_charge,
+            'total_amount' => $this->total_amount,
+            'total_product_cost' => $this->total_product_cost,
+            'total_returned_amount' => $this->total_returned_amount,
+            'total_final_amount' => $this->total_final_amount,
+            'status' => $this->status,
+            'total_merchant_commission' => $this->total_merchant_commission,
+            'notes' => $this->notes,
+            'refund_status' => $this->refund_status,
+            'feedback_text' => $this->feedback_text,
+            'feedback_number' => $this->feedback_number,
+            'created_at' => dateFormat($this->created_at, 1),
+            'details' => $this->orderDetails,
+            'user' => $this->user,
+            'address' => $this->address,
         ];
     }
+    
 }
